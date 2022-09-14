4cd19d404e5480e8b3f60baa5f8c52413d722a26
John <jwagner988@gmail.com>
2022-09-14 15:44:42 -0700

    updated link to resume

--- src/header.jsx
+++ src/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <p>jwagner988@gmail.com</p>
             <p className="desc">Experienced JavaScript engineer and open-source contributor. Currently developing full-stack applications with React, Node, Express, PostgreSQL, and MongoDB. </p>
             <ul>
-                <li><a href='https://myresumestorage.s3.us-west-1.amazonaws.com/UptoDateResume.pdf' target='_blank' rel="noopener noreferrer">Resume</a></li>
+                <li><a href='https://myresumestorage.s3.us-west-1.amazonaws.com/Resume.pdf' target='_blank' rel="noopener noreferrer">Resume</a></li>
                 <li><a href='https://github.com/jwagner988'>GitHub</a></li>
                 <li><a href="https://www.linkedin.com/in/jwagner988">Linkedin</a></li>
             </ul>
